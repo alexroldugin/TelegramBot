b1ffae7160d6bebb33b22dd99212e9862bdea539
Дима Сенько <iwannaexplore@gmail.com>
2021-06-15 18:09:38 +0300

    добавил более менее понятную страницу о результатах входа

---
+++ TelegramBot/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ContentResult Get()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = "<div>Bot is working. You can go <a href=\"https://t.me/test12323543435bot\">here</a></div>"
+            };
+        }
+    }
+}
